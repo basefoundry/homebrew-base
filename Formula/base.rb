895b81ee7e27c1fd45f1c8528ce088591f7c9ead
Ramesh Padmanabhaiah <codeforester@gmail.com>
2026-06-14 07:57:51 -0700

    Update Base formula to v1.0.0 (#18)
    * Update formula for Base v1.0.0
    
    * Add Base 1.0.0 Homebrew bottles
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/base.rb
+++ Formula/base.rb
@@ -1,17 +1,17 @@
 class Base < Formula
   desc "Workspace bootstrap and project environment orchestration tool"
   homepage "https://github.com/codeforester/base"
-  url "https://github.com/codeforester/base/archive/refs/tags/v0.4.4.tar.gz"
-  sha256 "2aca5e6f38d412269808edf1114cb7c25e3662b4fa276e7878a1b562ecd407d9"
-  version "0.4.4"
+  url "https://github.com/codeforester/base/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "07ec9edcf095fe17f6902fece88af00c398acb7fdd925497d477bdf6a41ed11e"
+  version "1.0.0"
   license "MIT"
   head "https://github.com/codeforester/base.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/codeforester/homebrew-base/releases/download/base-v0.4.4"
+    root_url "https://github.com/codeforester/homebrew-base/releases/download/base-v1.0.0"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e484453f9c757d099609322ee75c984a321c43209107c7e5381a8507f32a16f"
-    sha256 cellar: :any_skip_relocation, sequoia:       "5d0552a8326026ca54d1f7dfd918ae422ae63e16a7f816dbbeeef8b1c738f456"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dbdfc2f7d5c27329c39ea7791c8193760ffb248f92d65a1d17b3374c4d5d2b4b"
+    sha256 cellar: :any_skip_relocation, sequoia:       "94961a237330e28f2b455fa7121733bd71bf967091f16b3513a85c833d34c818"
   end
 
   depends_on "bash"
