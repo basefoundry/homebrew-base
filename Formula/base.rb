f4e3cab824b6676877c4a8d7601511c2a8d2aa14
Ramesh Padmanabhaiah <codeforester@gmail.com>
2026-06-16 00:00:57 -0700

    Update Base formula to v1.0.2

--- Formula/base.rb
+++ Formula/base.rb
@@ -1,18 +1,10 @@
 class Base < Formula
   desc "Workspace bootstrap and project environment orchestration tool"
   homepage "https://github.com/codeforester/base"
-  url "https://github.com/codeforester/base/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "a242333a096ef2f7f4b9608be8a309b8e3ed66c849d570d9fab3fd95d3377315"
+  url "https://github.com/codeforester/base/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "296fa4c0fca25a2cc453ac1d4c2fbd8cca6bf201af70ef29ce11e7b81b5d55f1"
   license "AGPL-3.0-or-later"
-  revision 2
   head "https://github.com/codeforester/base.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/codeforester/homebrew-base/releases/download/base-v1.0.1"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dfd871ce9212daac2261b27f44e2beca3949a5c733105816be0d1d11eb7ba1e5"
-    sha256 cellar: :any_skip_relocation, sequoia:       "1eb0eab8ae9f1938837539e8f514c1cb7ac9c6b89e70316aa7621ad3a7b82837"
-  end
 
   depends_on "bash"
   depends_on "python@3.13"
