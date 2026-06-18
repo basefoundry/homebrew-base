edb7dced0965c35d5f8a2d28a5bf2825eb0f34a4
Ramesh Padmanabhaiah <codeforester@gmail.com>
2026-06-18 15:03:43 -0700

    Update base-bash-libs formula to v0.2.0 (#43)

--- Formula/base-bash-libs.rb
+++ Formula/base-bash-libs.rb
@@ -1,17 +1,10 @@
 class BaseBashLibs < Formula
   desc "Reusable Bash libraries extracted from Base"
   homepage "https://github.com/codeforester/base-bash-libs"
-  url "https://github.com/codeforester/base-bash-libs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2a6407ec539310d3bc15db1b42a51e0a202ae4bdfe5dad36b95505867f75bf6a"
+  url "https://github.com/codeforester/base-bash-libs/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "0da893fcf763aa302913ff195545620c1f8783e0eb83619eb1bc0a33097a2cab"
   license "AGPL-3.0-or-later"
   head "https://github.com/codeforester/base-bash-libs.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/codeforester/homebrew-base/releases/download/base-v1.0.5"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "30d0442edb928e35ecfce3fa20eeb86d4930041294a88cd43817d2a15b31c6b2"
-    sha256 cellar: :any_skip_relocation, sequoia:       "8cf9a74a3b58cbabdbb12fba593fd31ca8aa4962dfe599e4a3b314b700f237b9"
-  end
 
   depends_on "bash"
 
@@ -40,10 +33,10 @@ class BaseBashLibs < Formula
       source "#{libexec}/lib/bash/std/lib_std.sh"
       import "#{libexec}/lib/bash/file/lib_file.sh"
       import "#{libexec}/lib/bash/git/lib_git.sh"
-      printf '%s\\n' "$(type -t run)" "$(type -t update_file_section)" "$(type -t git_get_current_branch)"
+      printf '%s\\n' "$BASE_BASH_LIBS_VERSION" "$(type -t std_run)" "$(type -t run)" "$(type -t update_file_section)" "$(type -t git_get_current_branch)"
     EOS
 
     bash = Formula["bash"].opt_bin/"bash"
-    assert_equal "function\nfunction\nfunction\n", shell_output("#{bash} #{testpath}/smoke.sh")
+    assert_equal "0.2.0\nfunction\nfunction\nfunction\nfunction\n", shell_output("#{bash} #{testpath}/smoke.sh")
   end
 end
