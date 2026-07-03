6cec75e35469c0eff6359f9e0205a2f9e693d291
Ramesh Padmanabhaiah <codeforester@gmail.com>
2026-07-02 23:39:51 -0700

    Update base-bash-libs formula to v2.0.0 (#66)

--- Formula/base-bash-libs.rb
+++ Formula/base-bash-libs.rb
@@ -1,21 +1,15 @@
 class BaseBashLibs < Formula
   desc "Reusable Bash libraries extracted from Base"
   homepage "https://github.com/basefoundry/base-bash-libs"
-  url "https://github.com/basefoundry/base-bash-libs/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "1a3e1ac86868cdda305e5eb314a434de2e1d904de6a733829a688f1c4bd82171"
+  url "https://github.com/basefoundry/base-bash-libs/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "161ea8ae71d605df86c55aeb444340aa6e09dba74b69acf541a8720a72748f38"
   license "Apache-2.0"
   head "https://github.com/basefoundry/base-bash-libs.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/basefoundry/homebrew-base/releases/download/base-v1.1.0"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e70232286b7f447d455333425d53ed8d657c9b9d783b4b829f959a6ae20ec34"
-    sha256 cellar: :any_skip_relocation, sequoia:       "6db726da89a61f28c7cdc060baef94717008c84b76ad20ecdbe46e494bddf83c"
-  end
 
   depends_on "bash"
 
   def install
+    bin.install "bin/base-bash"
     libexec.install "lib"
     libexec.install "VERSION"
     pkgshare.install "README.md", "CHANGELOG.md", "LICENSE", "NOTICE"
@@ -29,6 +23,9 @@ class BaseBashLibs < Formula
 
       Companion libraries live under:
         #{opt_libexec}/lib/bash
+
+      Run standalone scripts with the stdlib preloaded:
+        #!/usr/bin/env base-bash
     EOS
   end
 
@@ -36,6 +33,7 @@ class BaseBashLibs < Formula
     assert_path_exists libexec/"lib/bash/std/lib_std.sh"
     assert_path_exists libexec/"lib/bash/file/lib_file.sh"
     assert_path_exists libexec/"lib/bash/git/lib_git.sh"
+    assert_path_exists bin/"base-bash"
     assert_path_exists pkgshare/"LICENSE"
     assert_path_exists pkgshare/"NOTICE"
 
@@ -47,6 +45,21 @@ class BaseBashLibs < Formula
     EOS
 
     bash = Formula["bash"].opt_bin/"bash"
-    assert_equal "1.0.0\nfunction\nfunction\nfunction\nfunction\n", shell_output("#{bash} #{testpath}/smoke.sh")
+    assert_equal "2.0.0\nfunction\nfunction\nfunction\nfunction\n", shell_output("#{bash} #{testpath}/smoke.sh")
+
+    (testpath/"launcher.sh").write <<~EOS
+      #!/usr/bin/env base-bash
+
+      import_base_bash_lib str/lib_str.sh
+
+      main() {
+        local value="  launcher  "
+        str_trim value
+        printf '%s\\n' "$BASE_BASH_LIBS_VERSION" "$BASE_BASH_LIBS_STDLIB_LOADED" "$value" "$#"
+      }
+    EOS
+    chmod 0755, testpath/"launcher.sh"
+
+    assert_equal "2.0.0\n1\nlauncher\n1\n", shell_output("PATH=#{bin}:$PATH #{testpath}/launcher.sh arg")
   end
 end
