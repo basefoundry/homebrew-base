30e09f0e89d2b727f41631a7ca50f871503a3ad7
Ramesh Padmanabhaiah <codeforester@gmail.com>
2026-07-16 23:54:23 -0700

    Update Base formula to v1.7.0 (#81)
    * Update Base formula to v1.7.0
    
    * Add Base 1.7.0 Homebrew bottles
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/base.rb
+++ Formula/base.rb
@@ -1,16 +1,16 @@
 class Base < Formula
   desc "Workspace bootstrap and project environment orchestration tool"
   homepage "https://github.com/basefoundry/base"
-  url "https://github.com/basefoundry/base/archive/refs/tags/v1.6.1.tar.gz"
-  sha256 "1ad45fc1ae43401b68ef43b45362084363385d26611bfbf2df075d78a61f89d4"
+  url "https://github.com/basefoundry/base/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "cf37d7f907ce0883f609d03a423628c9838f89fa5b4b98f2992ff77ac3a70c1a"
   license "AGPL-3.0-or-later"
   head "https://github.com/basefoundry/base.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/basefoundry/homebrew-base/releases/download/base-v1.6.1"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "796545c88f9593e3dc59080575791ab8ca3ca6494f16f233dc58a13c53deeefa"
-    sha256 cellar: :any_skip_relocation, sequoia:       "060443de6e87ab98582910017e4eb9cba74460fe6301a85b4fc44f749fc058da"
+    root_url "https://github.com/basefoundry/homebrew-base/releases/download/base-v1.7.0"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a4e311e0eb8ccea47994c838f6e3af34f99c9fc6f0704a38453bb31b1daa0a77"
+    sha256 cellar: :any_skip_relocation, sequoia:       "936e4584f59b30e23024aafcc43d8836229a956de42c05cb58f7fc480e3fa60a"
   end
 
   depends_on "base-bash-libs"
